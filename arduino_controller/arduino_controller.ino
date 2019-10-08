bd2fd190cd54e482c95fae6e2894c1fd65ce139f
Harlen Bains <himalayanelixir@gmail.com>
2019-10-08 02:09:06 -0700

    Cleaned up code

--- arduino_controller/arduino_controller.ino
+++ arduino_controller/arduino_controller.ino
@@ -17,66 +17,16 @@ const byte numChars = 32;
 char receivedChars[numChars];
 bool newData = false;
 
-// initialize motors
-Servo myServo[NUMBER_MOTORS];
-// create a array of ports with the order: motor, counter, reset
-int ports[NUMBER_MOTORS][3] = {{2, 3, 4}, {5, 6, 7}};
-
-// string array of motor directions
-String motor_direction[NUMBER_MOTORS] = {""};
-
-// array of how many rotations a motor has to go
-byte motor_rotation_number[NUMBER_MOTORS] = {0};
-// array of new switch values
-byte motor_sensor_counter1[NUMBER_MOTORS] = {0};
-
-// array of old switch values
-byte motor_sensor_counter2[NUMBER_MOTORS] = {0};
-
-// 0 or 1 depending on the input signal
-byte input[NUMBER_MOTORS] = {0};
-// will range from 0 to the specified MAXIMUM
-int integrator[NUMBER_MOTORS] = {0};
-// cleaned-up version of the input signal
-byte output[NUMBER_MOTORS] = {0};
 
 void setup()
 {
   // setup serial port
   Serial.begin(9600);
 
-  // initialize all motor ports
-  Serial.println("Begining Initialization");
-  Serial.print("Motor Ports: ");
-  for (int i = 0; i < NUMBER_MOTORS; i++)
-  {
-    int x = ports[i][0];
-    Serial.print(x);
-    Serial.print(" ");
-    myServo[i].attach(x);
-  }
 
-  // initialize all counter ports
-  Serial.println("");
-  Serial.print("Counter Ports: ");
-  for (int i = 0; i < NUMBER_MOTORS; i++)
-  {
-    int x = ports[i][1];
-    Serial.print(x);
-    Serial.print(" ");
-    pinMode(x, INPUT_PULLUP);
-  }
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 
-  // initialize all reset ports
-  Serial.println("");
-  Serial.print("Reset Ports: ");
-  for (int i = 0; i < NUMBER_MOTORS; i++)
-  {
-    int x = ports[i][2];
-    Serial.print(x);
-    Serial.print(" ");
-    pinMode(x, INPUT_PULLUP);
-  }
   Serial.println("");
   Serial.println("<");
   Serial.println("Arduino is ready");
--- arduino_controller/arduino_controller_functions.ino
+++ arduino_controller/arduino_controller_functions.ino
@@ -1,119 +1,21 @@
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 
-// This file contains funcitons that process the data that is recieved from the
+// This file contains functions that process the data that is received from the
 // raspberry pi
 
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 
 void ProcessData() {
+  
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
+    delay(1000);                       // wait for half a second
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(1000);
+  }   
+   // turn the LED off 
 
-  /*   // variables
-  String recievedString = "";
-  String motor_rotation_string = "";
-  String motor_rotation_string2 = "";
-
-  recievedString = receivedChars;
-
-  // logic
-  motor_direction = getValue(recievedString, ',', 0);
-  motor_rotation_string = getValue(recievedString, ',', 1);
-  motor_rotation_number = motor_rotation_string.toInt();
-
-  //
-  if (motor_direction == "Up") {
-    myservo1.write(100);
-  }
-  else if (motor_direction == "Down") {
-    myservo1.write(80);
-  }
-
-  else if (motor_direction == "None") {
-    myservo1.write(90);
-  }
-  else if (motor_direction == "Reset") {
-    Reset1();
-  }
-  else
-  {
-    // Sends Error Message
-    Invalid();
-  }
-
-  // Print First Number
-  Serial.println("---------");
-  Serial.print("Motor 1: ");
-  Serial.println(motor_rotation_number);
-
-  while (true) {
-
-    motor_sensor_counter2 = motor_sensor_counter1;
-    checkswitch(motor_counter_port);
-    motor_sensor_counter1 = output;
-    delay(10);
-
-    if (motor_rotation_number == 0) {
-      myservo1.write(90);
-      break;
-    }
-
-    if (digitalRead(motor_reset_port) == 0) {
-      myservo1.write(90);
-      break;
-    }
-
-    if (motor_sensor_counter1 == 1 && motor_sensor_counter2 == 0) {
-      motor_rotation_number--;
-
-      // Print Number Every Time It Changes
-      Serial.print("Motor 1: ");
-      Serial.println(motor_rotation_number);
-    }
-  } */
-
-  // Send Finished Signal
   Finished();
 }
-
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
-
-void checkswitch(int switchPort) {
-  // /* Step 1: Update the integrator based on the input signal.  Note that the
-  //   integrator follows the input, decreasing or increasing towards the limits
-  //   as determined by the input state (0 or 1). */
-  // input = digitalRead(switchPort);
-
-  // if (input == 0)
-  // {
-  //   if (integrator > 0)
-  //     integrator--;
-  // }
-  // else if (integrator < MAXIMUM)
-  //   integrator++;
-
-  // /* Step 2: Update the output state based on the integrator.  Note that the
-  //   output will only change states if the integrator has reached a limit,
-  //   either 0 or MAXIMUM. */
-
-  // if (integrator == 0)
-  //   output = 0;
-  // else if (integrator >= MAXIMUM)
-  // {
-  //   output = 1;
-  //   integrator = MAXIMUM;  /* defensive code if integrator got corrupted */
-  // }
-
-  // /********************************************************* End of
-  // debounce.c */
-}
-
-/* void Reset1() {
-  myservo1.write(100);
-  while(true) {
-    if(digitalRead(motor_reset_port) == 0){
-      break;
-    }
-  }
-} */
